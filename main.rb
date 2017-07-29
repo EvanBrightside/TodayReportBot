488eb66d936157ec03215df5075eacc04aab8391
EvanBrightside <xvanx84@gmail.com>
2017-07-29 20:55:47 +0300

    Fix daily news

--- main.rb
+++ main.rb
@@ -62,8 +62,8 @@ def dailynews
 	items = Nokogiri::XML(open('https://meduza.io/rss/all', {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}))
 	dailynews = []
 	items.css('item')[0..5].map do |item|
-		title = "*#{item.at_css('title').children[1].text.upcase}*"
-		description = item.at_css('description').children[1].text
+		title = "*#{item.at_css('title').text.upcase}*"
+		description = item.at_css('description').text
 		link = "[Полная статья](#{item.at_css('link').text})"
 		dailynews << [title, description, link]
 	end
