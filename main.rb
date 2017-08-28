224554f3c1a6ab993c8e3248d83a941c2d3d3982
EvanBrightside <xvanx84@gmail.com>
2017-08-28 11:33:03 +0300

    fix live

--- main.rb
+++ main.rb
@@ -90,11 +90,11 @@ def live
 		soccer_rss = rss.items.select { |a| a.category.content =~ /#{liga}/ && a.pubDate.strftime("%d/%m/%Y") == Date.today.strftime("%d/%m/%Y") }
 	  soccerlive = []
 	  soccer_rss.each do |item|
-	    category = "*#{item.category.content.upcase}*"
-	    title = "`#{item.title}`"
-	    date = "`#{item.pubDate.strftime("%d/%m/%Y - %H:%M")}`"
-	    link = "[Ссылка на текстовую трансляцию](#{item.link})"
-	    soccerlive << [category, title, date, link]
+      category = "*#{item.category.content.upcase}*"
+      title = "`#{item.title}`"
+      date = "`#{item.pubDate.strftime("%d/%m/%Y - %H:%M")}`"
+      link = "[Ссылка на текстовую трансляцию](#{item.link})"
+      soccerlive << [category, title, date, link]
 	  end
 	  soccerlive.map { |a, s, d, f| [ a, s, d, ["#{f}\n"] ] }*"\n"
 	else
