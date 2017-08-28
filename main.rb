e15ce1032e9940ee62c0b2873d45e89a1ec128aa
EvanBrightside <xvanx84@gmail.com>
2017-08-28 11:34:33 +0300

    fix live

--- main.rb
+++ main.rb
@@ -83,23 +83,23 @@ def devby
 end
 
 def live
-	url = 'https://www.liveresult.ru/football/txt/rss'
-	if HTTParty.get(url).code == 200
-	  rss = RSS::Parser.parse(url)
-	  liga = %w{ Россия Италия Англия Германия Испания Франция Лига Международный Товарищеские Европы Мира }.join('|')
-		soccer_rss = rss.items.select { |a| a.category.content =~ /#{liga}/ && a.pubDate.strftime("%d/%m/%Y") == Date.today.strftime("%d/%m/%Y") }
-	  soccerlive = []
-	  soccer_rss.each do |item|
+  url = 'https://www.liveresult.ru/football/txt/rss'
+  if HTTParty.get(url).code == 200
+    rss = RSS::Parser.parse(url)
+    liga = %w{ Россия Италия Англия Германия Испания Франция Лига Международный Товарищеские Европы Мира }.join('|')
+    soccer_rss = rss.items.select { |a| a.category.content =~ /#{liga}/ && a.pubDate.strftime("%d/%m/%Y") == Date.today.strftime("%d/%m/%Y") }
+    soccerlive = []
+    soccer_rss.each do |item|
       category = "*#{item.category.content.upcase}*"
       title = "`#{item.title}`"
       date = "`#{item.pubDate.strftime("%d/%m/%Y - %H:%M")}`"
       link = "[Ссылка на текстовую трансляцию](#{item.link})"
       soccerlive << [category, title, date, link]
-	  end
-	  soccerlive.map { |a, s, d, f| [ a, s, d, ["#{f}\n"] ] }*"\n"
-	else
-		"Liveresult not avaliable now"
-	end
+    end
+    soccerlive.map { |a, s, d, f| [ a, s, d, ["#{f}\n"] ] }*"\n"
+  else
+    "Liveresult not avaliable now"
+  end
 end
 
 def transfers
